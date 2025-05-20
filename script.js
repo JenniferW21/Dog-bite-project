8087ac5363b8db3e09e604a1bb03a1d1a5b7c69d
JenniferW21 <jenniferwang570@gmail.com>
2025-05-20 17:25:32 +0000

    css

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ async function init(){
   info = await fetch(link);
   data = await info.json();
 }
+
+function displayLocation(){
+  let location = [40.754041, -73.872378];
+  let otput = document.getElementByID
+  showMap(location);
+}
+
+let map;
+
+function showMap(location){
+  if (map) {
+    map.remove();
+  }
+
+  map = L.map("map").setView(location, 14);
+
+  const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 18,
+    attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+  }).addTo(map);
+
+  let marker = L.marker(location).addTo(map);// ******** places marker on map
+}     
